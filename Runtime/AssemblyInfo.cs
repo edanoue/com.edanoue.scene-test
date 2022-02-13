f9b8d27e70c46a4a2414b6ed0d489c8059a61e9b
Minami Tomonobu <minami@edanoue.com>
2022-02-13 13:07:39 +0900

    add comment

--- Runtime/AssemblyInfo.cs
+++ Runtime/AssemblyInfo.cs
@@ -1,8 +1,6 @@
+// Copyright Edanoue, Inc. MIT License - see LICENSE.md
+
 using System.Runtime.CompilerServices;
 
-// この assembly "Edanoue.TestAPI.dll" に Internal アクセスできる Friend Assembries を定義しているファイル
-// InternalsVisibleTo を書き連ねて行ってください
-// このファイルにはいかなる実装も書かないでください
-
-// Edanoue.TestAPI.Editor
+// Friend assembries
 [assembly: InternalsVisibleTo("Edanoue.SceneTest.Editor")]
